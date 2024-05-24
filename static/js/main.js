557dc4bad1b5517d812299f64f4c7340c984f13d
rufusmccoot <skoverbeck@gmail.com>
2024-05-24 11:31:22 -0400

    add test sound button

--- static/js/main.js
+++ static/js/main.js
@@ -15,6 +15,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const themeSelect = document.getElementById('theme');
     themeSelect.addEventListener('change', (event) => {
         const selectedTheme = event.target.value;
+        const testSoundImage = document.getElementById('test-sound-image');
         document.body.classList.toggle('dark-mode', selectedTheme === 'dark');
+    if (selectedTheme === 'dark') {
+        testSoundImage.src = 'static/images/soundtest_modedark.png'; // Change to dark mode image
+    } else {
+        testSoundImage.src = 'static/images/soundtest_modelight.png'; // Change to default/light mode image
+    }
     });
-});cown
+
+    const testMySound = document.getElementById('test-sound-image');
+    testMySound.addEventListener('click', () => {
+        const sound = document.getElementById('notification-sound');
+        sound.play().catch(error => {
+            console.error('Error playing notification sound:', error);
+        });
+    });
+});
